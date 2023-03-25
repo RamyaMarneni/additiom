37d3e9b0c3cdf08964ad56ab8248c1110ab0ddd2
RamyaMarneni <125952449+RamyaMarneni@users.noreply.github.com>
2023-03-25 11:11:39 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,12 @@
+public class add {
+    public static void main(String[] args) {
+        {
+            int n1=225,n2=115,sum;
+            sum=n1+n2;
+            System.out.println("the sum of numbers is:"+sum);
+
+        }
+        
+    }
+    
+}
